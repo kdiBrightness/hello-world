3b2ebab8a159371dc51800f82f3f2638ccbcc843
kdiBrightness <khadyd92@gmail.com>
2015-11-29 16:01:07 +0100

    ajout d'une ligne de print

---
+++ test/src/test/Helloword.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Helloword {
+
+	public static void main(String[] args) {
+		System.out.println("slt tu mas manquei");
+		System.out.println("slt tu mas manquei 2fois");	
+	}
+
+}
